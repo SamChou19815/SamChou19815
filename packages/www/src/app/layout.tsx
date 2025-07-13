626ae3f9410d32cf37a0d2ff4b97f68fda7ac607
Sam Zhou <sam@developersam.com>
2025-07-13 10:37:31 -0400

    [website] Remove google analytics
    Summary:
    
    Test Plan:

--- packages/www/src/app/layout.tsx
+++ packages/www/src/app/layout.tsx
@@ -1,13 +1,6 @@
 import "../lib/common.css";
 
 import type { Metadata } from "next";
-import Script from "next/script";
-
-const GA_ID = "G-K50MLQ68K6";
-const GA_INLINE_SCRIPT = `window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
-gtag('config', '${GA_ID}');`;
 
 export const metadata: Metadata = {
   icons: ["/logo.png", { rel: "shortcut icon", url: "/favicon.ico" }],
@@ -31,12 +24,6 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      {process.env.NODE_ENV === "production" && (
-        <Script src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`} />
-      )}
-      {process.env.NODE_ENV === "production" && (
-        <Script id="google-analytics">{GA_INLINE_SCRIPT}</Script>
-      )}
       <body>{children}</body>
     </html>
   );
